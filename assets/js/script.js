d645b2af45ad49b654ecc40de7785948956cf3f1
mattkohl82 <mattkohl82@gmail.com>
2020-04-10 23:31:14 -0500

    setting up css

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+var quizQuestions = [
+    {
+        question: "The answer is A?",
+        answers: {
+            a: "a",
+            b: "b",
+            c: "c",
+            d: "d"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question : "The Answer is B?",
+        answers: {
+            a: "a",
+            b: "b",
+            c: "c",
+            d: "d"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "The answer is c?",
+        answers: {
+            a: "a",
+            b: "b",
+            c: "c",
+            d: "d"
+        },
+        correctAnswer: "c"
+    },
+
+    {
+        question: "The answer is d?",
+        answers: {
+            a: "a",
+            b: "b",
+            c: "c",
+            d: "d"
+        },
+        correctAnswer: "d"
+    },
+
+]
+
+console.log(quizQuestions);
